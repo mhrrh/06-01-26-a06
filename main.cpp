fcf33e89a00c9843f60a82c6942cb515d574b2f5
mhrrh <madison.harrah@pcc.edu>
2026-06-01 21:54:01 +0000

    2

--- main.cpp
+++ main.cpp
@@ -76,9 +76,67 @@ int main(int argc, char* argv[]) {
               cout << "Enter coins - 5, 10, or 25 only: ";
               cin >> coin;
             }
-            if 
+            if (coin != COIN_END){
+              balance = balance + coin;
+            }
+    } while (coin != COIN_END);
+
+    cout << end;
+    cout << "Your balance is $" << balance / 100.0 << endl << endl;
+
+    cout << "Please pick an option ($0.25 each):" << endl;
+    cout << "     C/c: Coffee" << endl;
+    cout << "     T/t: Tea" << endl;
+    cout << "     Q/q: Quit" << endl;
+    cout << ">> ";
+    cin >> choice;
+
+    while (choice != 'C' && choice != 'c' && choice != 'T' && choice != 't' && choice != 'Q' && choice != 'q'){
+      cout << "Invalid option! Please choose a valid option!" << endl;
+      cout << ">> ";
+      cin >> choice;
     }
+    if (choice == 'Q' || choice == 'q'){
+      totalCost = 0;               
+      change = balance;
+      cout << endl;
+      cout << "Your total is $" << totalCost / 100.0 << endl;
+      cout << "Your balance is $" << change / 100.0 << endl << endl;
+
+      keepGoing = false;
+    }
+    else {
+      cout << endl;
+      cout << "How many would you like?" << endl;
+      cout << ">> ";
+      cin >> quantity;
+
+      while (cin.fail() || quantity <= 0){
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        cout << "Invalid option!" << endl;
+        cout << "How many items would you like?" << endl;
+        cout << ">> ";
+        cin >> quantity;
+      }
+      totalCost = ITEM_COST * quantity;
+
+      cout << endl;
+      cout << "Your total is $" << totalCost / 100.0 << endl;
+
+      if (balance >= totalCost){
+        change = balance - totalCost;
+        cout << "Your balance is $" << change / 100.0 << endl << endl;
+        keepGoing = false;
+      }
+      else {
+        cout << "Your balance is $" << change / 100.0 << endl;
+        cout << "Not enough change! Please add more coins." << endl << endl;
+      }
+     }
   }
+  cout << "Thank you for using my Vending Machine Program!" << endl;
 
 
   return 0;
@@ -88,7 +146,7 @@ int main(int argc, char* argv[]) {
 
 
 // ------------- DESIGN -------------
-/* 
+/*              
 Program Name:
 
 Program Description:
